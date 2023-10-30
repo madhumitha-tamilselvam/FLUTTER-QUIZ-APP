70c16c1a11513a7af4b6b9bf6376d646576a8faf
MADHUMITHA T <126783727+madhumitha-tamilselvam@users.noreply.github.com>
2023-10-30 20:31:55 +0530

    Add files via upload

---
+++ FLUTTER QUIZ APP/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color correct = Color.fromARGB(255, 80, 223, 85);
+const Color incorrect = Color.fromARGB(255, 248, 53, 39);
+const Color neutral = Color.fromARGB(236, 255, 255, 255);
+const Color background = Color.fromARGB(255, 12, 105, 128);
